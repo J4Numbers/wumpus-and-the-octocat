24a8643ebb98f60e9ab63689f3d9a0aa52a4e422
Matthew D. Ball <m4numbers@gmail.com>
2019-06-18 21:03:33 +0100

    chore(app): minor tidy-up of code and apps

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -9,11 +8,6 @@ bot.init();
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
