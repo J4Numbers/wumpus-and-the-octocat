e21c989de7d7b1f0f3cfc80630da77eea9a3dde6
J4Numbers <j4numbers@gmail.com>
2019-06-18 20:59:25 +0100

    feat(initial): existing code import

---
+++ bot.js
@@ -0,0 +1,71 @@
+const config = require('config');
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+function _init() {
+    client.login(config.get('discord.token'));
+}
+
+function ping(payload) {
+    return "Ping request from repository"
+}
+
+function push(payload) {
+    var details;
+    if (payload.base_ref == null) {
+        details = `New push event on branch '${payload.ref}' with commits:\n`;
+    } else {
+        details = `New merge event from branch '${payload.base_ref}' `
+            + `onto branch '${payload.ref}' with fast-forwarded commits:\n`;
+    }
+    for (var i in payload.commits) {
+        details += `\t${payload.commits[i].timestamp} :: `
+            + `${payload.commits[i].author["username"]} :: `
+            + `${payload.commits[i].message}\n`;
+    }
+    return details;
+}
+
+function create(payload) {
+    return `New branch ${payload.ref} created by ${payload.sender.login}\n`
+}
+
+function _event(eventType, jsonData) {
+    var message = `Event on repository: ${jsonData.repository.html_url}\n` +
+              `Updated at ${jsonData.repository.updated_at} by user ${jsonData.sender.login}`;
+    var details;
+
+    switch (eventType){
+        case "ping": {
+            details = ping(jsonData);
+            break;
+        }
+        case "push": {
+            details = push(jsonData);
+            break;
+        }
+        case "create": {
+            details = create(jsonData);
+            break;
+        }
+        default: {
+            details = ""
+        }
+    }
+
+    for (var channelId in config.get('discord.registered_channels')) {
+        client.channels.find("name", "bot-errors").send(message + "\n\n" + details);
+        console.log(message + "\n\n" + details);
+    }
+}
+
+
+module.exports = {
+    init: _init,
+    event: _event,
+};
