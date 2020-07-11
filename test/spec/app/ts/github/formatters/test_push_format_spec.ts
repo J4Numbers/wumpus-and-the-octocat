cf67d2a2d1ee158bb8a49a87489bbde8cb81553c
J4Numbers <j4numbers@gmail.com>
2020-07-11 04:29:25 +0100

    chore(push): added dummy commit objects to the dummy push

--- test/spec/app/ts/github/formatters/test_push_format_spec.ts
+++ test/spec/app/ts/github/formatters/test_push_format_spec.ts
@@ -7,41 +7,37 @@ const dummyPush = require("../../../../../../data/dummy-push")
 
 describe("GitHub Webhook push formatter", function () {
    it("Should return a default sentence when no commits are found", function () {
+       const copyPush = dummyPush;
+       copyPush.commits = [];
        expect(GithubFormatters.push(dummyPush)).to.equal('New push event on branch \'refs/tags/simple-tag\' with commits:\n');
    });
 
    it("Should create a new line when commits are included in the data", function () {
       const copyPush = dummyPush;
+      copyPush.commits = [];
       copyPush.commits.push({
           "id": "039fb150a287031ae2404320680a41fbf1be85e1",
           "tree_id": "5682731b92f0b03a0f1339f34e682ca01014d67a",
           "distinct": true,
-          "message": "start writing in tests for typescript",
-          "timestamp": "2020-07-11T04:03:53+01:00",
-          "url": "https://github.com/J4Numbers/wumpus-and-the-octocat/commit/039fb150a287031ae2404320680a41fbf1be85e1",
+          "message": "first commit",
+          "timestamp": "2020-07-01T00:00:00+01:00",
+          "url": "https://github.com/Codertocat/Hello-World/commit/039fb150a287031ae2404320680a41fbf1be85e1",
           "author": {
-              "name": "J4Numbers",
-              "email": "j4numbers@gmail.com",
-              "username": "J4Numbers"
+              "name": "Codertocat",
+              "email": "21031067+Codertocat@users.noreply.github.com",
+              "username": "Codertocat"
           },
           "committer": {
-              "name": "J4Numbers",
-              "email": "j4numbers@gmail.com",
-              "username": "J4Numbers"
+              "name": "Codertocat",
+              "email": "21031067+Codertocat@users.noreply.github.com",
+              "username": "Codertocat"
           },
           "added": [
-              "test/.ts.mocharc.js",
-              "test/spec/app/ts/github/formatters/test_create_format_spec.ts",
-              "test/spec/app/ts/github/formatters/test_ping_format_spec.ts",
-              "test/spec/app/ts/github/formatters/test_push_format_spec.ts"
+              "hello-world"
           ],
           "removed": [],
-          "modified": [
-              "package-lock.json",
-              "package.json",
-              "src/ts/github/formatters/push.ts"
-          ],
+          "modified": []
       });
-      expect(GithubFormatters.push(copyPush)).to.contain('\t2020-07-11T04:03:53+01:00 :: J4Numbers :: start writing in tests for typescript\n');
+      expect(GithubFormatters.push(copyPush)).to.contain('\t2020-07-01T00:00:00+01:00 :: Codertocat :: first commit\n');
    });
 });
