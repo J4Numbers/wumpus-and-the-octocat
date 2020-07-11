1072f12f2573d6f9413bfcc82d059c85963ec4be
J4Numbers <j4numbers@gmail.com>
2020-07-11 04:13:03 +0100

    test(ts): use a real commit object for testing

--- src/ts/github/formatters/push.ts
+++ src/ts/github/formatters/push.ts
@@ -1,16 +1,15 @@
 import Webhooks from "@octokit/webhooks";
 
 export function format(payload: Webhooks.WebhookPayloadPush): string {
-    let details;
+    let details: string;
     if (payload.base_ref == null) {
         details = `New push event on branch '${payload.ref}' with commits:\n`;
     } else {
         details = `New merge event from branch '${payload.base_ref}' `
             + `onto branch '${payload.ref}' with fast-forwarded commits:\n`;
     }
-    for (let i in payload.commits) {
-        details += `\t${payload.commits[i].author["username"]} :: `
-            + `${payload.commits[i].message}\n`;
-    }
+    payload.commits.forEach((commit) => {
+        details += `\t${commit.timestamp} :: ${commit.author.username} :: ${commit.message}\n`
+    });
     return details;
 }
